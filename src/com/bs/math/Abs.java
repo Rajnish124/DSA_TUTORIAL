ff21a0dbc4e8b6bfa50e8ebd41506c113c244808
Rajnish <krajmd01@gmail.com>
2023-07-30 17:20:51 +0530

    revert changes

--- src/com/bs/math/Abs.java
+++ src/com/bs/math/Abs.java
@@ -2,14 +2,7 @@ package com.bs.math;
 
 public class Abs {
 	
-	public static void main(String[] args) {
-		
-		System.out.println("Hello");
-		
-		System.out.println("Ram");
-		
-		
-	}
+	//this is a new added file
 	
 
 }
