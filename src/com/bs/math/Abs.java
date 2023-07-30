8a37206713f438226692ea05facae70f18173678
Rajnish124 <60813155+Rajnish124@users.noreply.github.com>
2023-07-30 22:41:02 +0530

    Update Abs.java

--- src/com/bs/math/Abs.java
+++ src/com/bs/math/Abs.java
@@ -5,7 +5,11 @@ public class Abs {
 	//this is a new added file
 	public void playFootball()
 	{
-		System.out.println("stash example");  
+		System.out.println("stash example"); 
+		ArrayList<String> arr=new ArrayList<>();
+		arr.add("ZZZ");
+		arr.add("XXX");
+		arr.add("AAA");
 	}
 
 }
